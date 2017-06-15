316f221944d8c42ddf1a7e6f7ee7f54f772a9082
Administrator <Administrator@20170419-141915>
2017-06-15 11:23:07 +0800

    update

--- src/com/design/pattern/factory/MyTest.java
+++ src/com/design/pattern/factory/MyTest.java
@@ -4,6 +4,7 @@ public class MyTest {
 	
 	public static void main(String[] args) {
 		
+		//凡是出现了大量的产品需要创建，并且具有共同的接口时，可以通过工厂方法模式进行创建。
 		SendFactory sendFactory = new SendFactory();
 		ISender sender1 = sendFactory.produce("mail"); //如果传入的字符串有误，不能正确创建对象
 		sender1.send();
